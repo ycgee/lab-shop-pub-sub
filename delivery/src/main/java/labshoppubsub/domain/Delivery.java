12a824ff46079c55ba473450d06ea66e120e3d6b
ycgee <youngsteel@gmail.com>
2022-11-13 00:08:58 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -0,0 +1,69 @@
+package labshoppubsub.domain;
+
+import labshoppubsub.DeliveryApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Delivery_table")
+@Data
+
+public class Delivery  {
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private String customerId;
+    
+    
+    
+    
+    
+    private Long ordderId;
+
+
+    public static DeliveryRepository repository(){
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(DeliveryRepository.class);
+        return deliveryRepository;
+    }
+
+
+
+
+    public static void addToDeliveryList(OrderPlaced orderPlaced){
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+
+         });
+        */
+
+        
+    }
+
+
+}
